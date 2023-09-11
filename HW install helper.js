486a3168ecd5467d597419118d421e6205f5bddb
Josh Chen <88011204+joshm3u@users.noreply.github.com>
2023-09-11 12:04:21 +1000

    Update and rename HW install helper to HW install helper.js

--- HW install helper.js
+++ HW install helper.js
@@ -4,8 +4,8 @@
 // @version      0.1
 // @description  Collect input and fill the HW installation MCM template
 // @author       chengng@
-// @match      https://mcm.amazon.com/cms/new?from_template=7b61ac86-0baa-44af-b9f5-be930912b72d
-// @updateURL  https://raw.githubusercontent.com/joshm3u/cable-patch-helper/main/HW%20install%20helper.js
+// @match       https://mcm.amazon.com/cms/new?from_template=0d640ded-d096-48a6-b3f5-c7c2d5fa76a7
+// @updateURL   https://raw.githubusercontent.com/joshm3u/cable-patch-helper/main/HW%20install%20helper.js
 // @downloadURL https://raw.githubusercontent.com/joshm3u/cable-patch-helper/main/HW%20install%20helper.js
 // @grant        none
 // ==/UserScript==
@@ -27,24 +27,24 @@ REVISION HISTORY:
         const userInput5 = prompt('Enter the text to replace "05_justification_for_change_and_purpose_of_project":');
         const userInput6 = "No"; // Predefined input for 06_Is_this_MCM_a_continuation_of_a_previous_MCM_if_yes_list_them_below_and_attach_them_in_related_items
         const userInput7 = prompt('Enter the text to replace "07_primary_sim_URL":');
-        const userInput8 = "N/A"; // Predefined input for 08_mobius_link
-        const userInput9 = "N/A"; // Predefined input for 09_NARG_tickets_Link
-        const userInput10 = prompt('Enter the text to replace "10_number_of_affected_devices":');
-        const userInput11 = prompt('Enter the text to replace "11_number_of_connections_to_be_patched_or_validated":');
-        const userInput12 = "N/A"; // Predefined input for 12_is_two_person_verification_required_for_this_activity_if_yes_explain
-        const userInput13 = "N/A"; // Predefined input for 13_alias_of_Performer
-        const userInput14 = "N/A"; // Predefined input for 14_alias_of_Verifier
-        const userInput15 = "Manual_Input_Here"; // Predefined input for 15_list_all_hostnames_locations_production_status_and_if_in_scope_of_this_activity
-        const userInput16 = "Manual_Input_Here"; // Predefined input for 16_patch_panels_and_locations_if_applicable
+        const userInput8 = prompt('Enter the text to replace "08_rspc_ticket":');
+        const userInput9 = prompt('Enter the text to replace "09_Power_Prep_and_Power_up_ticket":');
+        const userInput10 = "N/A"; // Predefined input for 10_NARG_tickets_Link
+        const userInput11 = prompt('Enter the text to replace "11_number_of_affected_devices":');
+        const userInput12 = "Manual_Input_Here"; // Predefined input for 12_Quantity_Manufacturer_Model_rack_position
+        const userInput13 = "No"; // Predefined input for 13_is_two_person_verification_required_for_this_activity_if_yes_explain
+        const userInput14 = "N/A"; // Predefined input for 14_alias_of_Performer
+        const userInput15 = "N/A"; // Predefined input for 15_alias_of_Verifier
+        const userInput16 = "Manual_Input_Here"; // Predefined input for 16_list_all_hostnames_locations_production_status_and_if_in_scope_of_this_activity
         const userInput17 = "normal business hour for cable patching"; //Predefined input for 17_why_is_this_the_correct_time_and_day_to_complete_the_mcm
         const userInput18 = "Yes"; // Predefined input for 18_are_there_any_related_MCMs_that_must_be_completed_before_this_change_occurs
         const userInput19 = "Yes, Please_paste_NDE_cutsheet_here"; // Predefined input for "; // Predefined input for 19_are_the_Cut_sheet_MCMs_in_a_fully_approved_state_if_yes_list_them_below_and_attach_them_in_related_items
         const userInput20 = "non-intrusive"; // Predefined input for 20_if_this_MCM_is_intrusive_what_services_will_be_affected
         const userInput21 = "Yes"; // Predefined input for 21_have_we_segmented_this_change_into_small_enough_stages_to_minimize_the_blast_radius_accelerate_impact_detection_and_ease_rollback
-        const userInput22 = "Cable patching is Tier 3 according to https://w.amazon.com/bin/view/GlobalEdge/Documentation/MCM/"; // Predefined input for 22_what_is_the_justification_for_this_tier_level
+        const userInput22 = "New Rack Deployments within Edge sites (w/o connecting to live devices/panels) is Tier 4 as per wiki:\n https://w.amazon.com/bin/view/GlobalEdge/Documentation/MCM/"; // Predefined input for 22_what_is_the_justification_for_this_tier_level
         const userInput23 = "non-intrusive"; // Predefined input for 23_list_every_step_that_you_will_take_during_your_activity
-        const userInput24 = "Impact could cause interface flapping which could cause customer impact"; // Predefined input for 24_what_could_happen_if_this_change_causes_impact
-        const userInput25 = "Fiber will be patched into NON-LIVE and/or LIVE racks which existing service might be jarred, bumped, cut, bent, or otherwise disrupted to the point of causing interface flapping which could cause customer impact."; // Predefined input for 25_where_are_the_most_likely_places_this_change_will_fail
+        const userInput24 = "Impact could cause an outage to neighboring devices within the rack as listed in the Hostname section"; // Predefined input for 24_what_could_happen_if_this_change_causes_impact
+        const userInput25 = "Hardware will be installed in a rack position that is potentially live. Extreme care will be used to prevent impact to any neighboring devices."; // Predefined input for 25_where_are_the_most_likely_places_this_change_will_fail
         const userInput26 = "1.) In the event that impact is detected by EDGE OPS oncall, EDGE Projects will immediately halt work.\n2.) Edge Projects will standby for directions provided by EDGE OPS oncall to assist in mitigation of impact.\n3.) EDGE Projects will perform said directed activities and inform EDGE OPS oncall of expected recovery.\n4.) EDGE Projects will then confirm recovery and re-evaluate if work can re-continue alongside EDGE OPS."; // Predefined input for 26_describe_rollback_plan
 
 
@@ -61,8 +61,8 @@ REVISION HISTORY:
             pasteUserInputInTextarea('templateVariablesTable', 'templateVariables[{{05_justification_for_change_and_purpose_of_project}}]', userInput5);
             pasteUserInputInTextarea('templateVariablesTable', 'templateVariables[{{06_Is_this_MCM_a_continuation_of_a_previous_MCM_if_yes_list_them_below_and_attach_them_in_related_items}}]', userInput6);
             pasteUserInputInTextarea('templateVariablesTable', 'templateVariables[{{07_primary_sim_URL}}]', userInput7);
-            pasteUserInputInTextarea('templateVariablesTable', 'templateVariables[{{08_mobius_link}}]', userInput8);
-            pasteUserInputInTextarea('templateVariablesTable', 'templateVariables[{{09_NARG_tickets_Link}}]', userInput9);
+            pasteUserInputInTextarea('templateVariablesTable', 'templateVariables[{{08_rspc_ticket}}]', userInput8);
+            pasteUserInputInTextarea('templateVariablesTable', 'templateVariables[{{09_Power_Prep_and_Power_up_ticket}}]', userInput9);
             pasteUserInputInTextarea('templateVariablesTable', 'templateVariables[{{10_number_of_affected_devices}}]', userInput10);
             pasteUserInputInTextarea('templateVariablesTable', 'templateVariables[{{11_number_of_connections_to_be_patched_or_validated}}]', userInput11);
             pasteUserInputInTextarea('templateVariablesTable', 'templateVariables[{{12_is_two_person_verification_required_for_this_activity_if_yes_explain}}]', userInput12);
@@ -91,8 +91,8 @@ REVISION HISTORY:
             replacePlaceholderInTextarea('description', '{{05_justification_for_change_and_purpose_of_project}}', userInput5);
             replacePlaceholderInTextarea('description', '{{06_Is_this_MCM_a_continuation_of_a_previous_MCM_if_yes_list_them_below_and_attach_them_in_related_items}}', userInput6);
             replacePlaceholderInTextarea('description', '{{07_primary_sim_URL}}', userInput7);
-            replacePlaceholderInTextarea('description', '{{08_mobius_link}}', userInput8);
-            replacePlaceholderInTextarea('description', '{{09_NARG_tickets_Link}}', userInput9);
+            replacePlaceholderInTextarea('description', '{{08_rspc_ticket}}', userInput8);
+            replacePlaceholderInTextarea('description', '{{09_Power_Prep_and_Power_up_ticket}}', userInput9);
             replacePlaceholderInTextarea('description', '{{10_number_of_affected_devices}}', userInput10);
             replacePlaceholderInTextarea('description', '{{11_number_of_connections_to_be_patched_or_validated}}', userInput11);
             replacePlaceholderInTextarea('description', '{{12_is_two_person_verification_required_for_this_activity_if_yes_explain}}', userInput12);
